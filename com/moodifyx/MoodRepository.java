3081ac38298cbb7745174f4c2a8d5b5b04d38199
LungeloGit <lungelo.ntaka2002@gmail.com>
2025-09-03 09:19:47 +0200

    Mood Based Music App
    Choose Music based on your mood
    2025/09/03

---
+++ com/moodifyx/MoodRepository.java
@@ -0,0 +1,59 @@
+package com.moodifyx;
+
+import java.util.*;
+
+import javax.swing.SortingFocusTraversalPolicy;
+public class MoodRepository {
+    private final Map<String, List<Song>> moodSongs = new HashMap<>();
+
+    public MoodRepository(){
+        initializeMoodSongs();
+
+    }
+
+
+    private void initializeMoodSongs(){
+        Map<String, String[]> moodFiles = Map.of(
+          "Happy", new String[]{"happygirl.wav","happygirlie.wav"},
+          "Angry", new String[]{"angrygirl.wav","angrygirlie.wav"},
+          "Chill", new String[]{"chillgirl.wav","chillgirlie.wav"},
+          "Mysterious", new String[]{"mysteriousgirl.wav","myseriousgirlie.wav"},
+          "Moody", new String[]{"moodygirl.wav","moodygirlie.wav"},
+          "Practice", new String[]{"practicegirl.wav","practicegirlie.wav"},
+          "Romantic", new String[]{"romanticgirl.wav","romanticgirlie.wav"},
+          "Peacceful", new String[]{"peacefulgirl.wav","peacefulgirlie.wav"},
+          "Positive", new String[]{"positivegirl.wav","positivegirlie.wav"}
+        );
+        for(Map.Entry<String, String[]> entry : moodFiles.entrySet()){
+            addMood(entry.getKey(), entry.getValue());
+        }
+    }
+    private String formatTitle(String fileName){
+        String base = fileName.replace(".wav", "");
+        if (base.endsWith("girl")) {
+            base= base.replace("girl", "");
+
+        } else if (base.endsWith("girlie")) {
+            base=base.replace("girlie", "");
+        }
+        base= base.trim();
+        return base.substring(0,1).toUpperCase()+
+        base.substring(1)+" Vibe";
+
+            }
+      private void addMood(String mood, String[] files){
+        List<Song> songs = new ArrayList<>();
+        for (String file :files){
+            songs.add(new Song(formatTitle(file),"music/"+file));
+        }
+        moodSongs.put(mood, songs);
+      }  
+      public List<Song> getSongsforMood(String mood){
+        return moodSongs.getOrDefault(mood, List.of(new Song("Default Vibe",
+        "music/happygirl.wav")));
+      }
+      public Set<String> getAllMoods(){
+        return moodSongs.keySet();
+      }
+      
+}
